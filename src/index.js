4d699044c5b9bde5ee4865d89e672fcb0413f0c1
Gustavo Mattos <gustavomattos777@gmail.com>
2022-10-11 01:03:21 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+// localhost:3333
+
+app.get('/', (request, response) => {
+    return response.json({ message:"Hello Word ignite!" });
+});
+
+app.listen(3333);
